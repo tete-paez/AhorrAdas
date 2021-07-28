c2146044d5a9cdd0f2630312b516c353ccfc0965
tetepaez <tete.paez.007@gmail.com>
2021-07-28 07:00:16 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+
+//PARA QUE APAREZCA Y DESAPAREZCA EL SECTION DE 
+panelAsideTxt.style = 'display: none';
+btnTxt.addEventListener('click', () => {
+  panelAsideTxt.style = 'display: inline';
+  panelAsideImg.style = 'display: none';
+})
