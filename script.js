32dc1eec13385c534ab3ae87cccd97d9bc1586ba
tetepaez <tete.paez.007@gmail.com>
2021-08-09 19:47:02 -0300

    my functions

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
+//inicializacion de la aplicacion vamos a buscar lo guardado en el local storage 
+const init = () => {
+  cont storage = JSON.parse(localStorage.getItem('everythingInStorage'));
 
-//PARA QUE APAREZCA Y DESAPAREZCA EL SECTION DE 
-panelAsideTxt.style = 'display: none';
-btnTxt.addEventListener('click', () => {
-  panelAsideTxt.style = 'display: inline';
-  panelAsideImg.style = 'display: none';
-})
+
+
+
+
+// <!--++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++-->
+// <!--+++++++++++++++++++++++  del profe  +++++++++++++++++++++-->
+//  <!--++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++-->
+
+
+var getStorage = function () {
+  var locStor = JSON.parse(localStorage.getItem('todo-storage'));
+  if (!locStor) {
+      locStor = {
+          categories: [],
+          users: [],
+          tasks: []
+      };
+  }
+  return locStor;
+};
+var usuario1 = {
+  id: 1,
+  email: 'asd@asd.com',
+  name: 'Adrián',
+  pass: 'asd123'
+};
+for (var prop in usuario1) {
+  console.log(prop, usuario1[prop]);
+}
+var props = Object.keys(usuario1);
+for (var _i = 0, props_1 = props; _i < props_1.length; _i++) {
+  var prop = props_1[_i];
+  console.log(prop, usuario1[prop]);
+}
+
+}
